8bf19f6aeb823f5ce6359a3cb19135e9a8a26f53
doreenyan <yan.doreen@gmail.com>
2016-04-03 18:07:47 -0400

    need subset

--- src/queues/RandomizedQueue.java
+++ src/queues/RandomizedQueue.java
@@ -9,87 +9,13 @@ import java.util.NoSuchElementException;
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Node first, last;
+    private Node currentNode = first;
     private int count;
 
     public RandomizedQueue() {
 
     }                           // construct an empty deque
 
-    private class Node {
-        Item item = null;
-        Node next, previous = null;
-    }
-
-    private class QueueIterator implements Iterator<Item> {
-        Node current = first;
-        Node newLast, newFirst = null;
-
-
-        public QueueIterator() {
-            Item[] scramble = new Item[count];
-            for (int i = 0; i < count; i++){
-                scramble[i] = current.item;
-                if (current.next != null){
-                    current = current.next;
-                }
-                else {
-                    current = first;
-                }
-            }
-            for (int i = 0; i < count; i++){
-                int choice = (int)(Math.random()*(count-i))+i;
-                Item ite = scramble[i];
-                scramble[i] = scramble[choice];
-                scramble[choice] = ite;
-
-                Node dad = new Node();
-                dad.next=null;
-                if (i==0){
-                    dad.previous=null;
-                    dad.item = ite;
-                    newFirst = dad;
-                }
-                else if (i==count-1){
-                    Node pre = current;
-                    pre.next = dad;
-                    dad.previous = pre;
-                    dad.item = ite;
-                    newLast = dad;
-                    current = first;
-                }
-                else {
-                    Node pre = current;
-                    pre.next = dad;
-                    dad.previous = pre;
-                    dad.item = ite;
-                    current = current.next;
-
-                }
-            }
-            first = newFirst;
-            last = newLast;
-        }
-
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-
-
-        }              // removeFirst and return the item from the front
-
-        public Item next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
-
-    }
 
     public boolean isEmpty() {
         if (count == 0) {
@@ -106,7 +32,6 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     }                       // return the number of items on the deque
 
-
     public void enqueue(Item item) {
         Node oldLast = last;
         last = new Node();
@@ -114,8 +39,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         last.next = null;
         if (isEmpty()) {
             first = last;
-        }
-        else {
+        } else {
             oldLast.next = last;
         }
         last.previous = oldLast;
@@ -124,81 +48,135 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }         // addLast the item to the end
 
     public Item dequeue() {
-        int random = (int)(Math.random()*count);
-        if(random==0 || count==1 || count == 0) {
+        if (isEmpty()) {
+            throw new UnsupportedOperationException();
+        }
+        int random = (int) (Math.random() * count);
+        if (random == 0) {
             Item item = first.item;
-            first = first.next;
+            if (count!=1) {
+                first = first.next;
+            }
+            first.previous = null;
+            count--;
             if (isEmpty()) {
                 last = null;
             }
-            count--;
             return item;
-        }
-        else if (random == count-1){
+        } else if (random == count - 1) {
             Item temp = last.item;
             last = last.previous;
             last.next = null;
+            count--;
             if (isEmpty()) {
                 last = null;
-
             }
-            count--;
             return temp;
-        }
-        else {
-
+        } else {
             Node temp = first;
-            for (int i = 0; i<random; i++) {
+            for (int i = 0; i < random; i++) {
                 temp = temp.next;
             }
             Item ite = temp.item;
             Node pre = temp.previous;
             Node nex = temp.next;
-            temp = null;
             pre.next = nex;
             nex.previous = pre;
             count--;
             return ite;
         }
 
-
     }              // removeFirst and return the item from the front
 
-    public Item sample()  {
-        int random = (int)(Math.random()*count);
-        if(random==0 || count==1 || count == 0) {
+    public Item sample() {
+        int random = (int) (Math.random() * count);
+        if (random == 0 || count == 1 || count == 0) {
             return first.item;
-        }
-        else if (random == count-1){
+        } else if (random == count - 1) {
             return last.item;
-        }
-        else {
+        } else {
             Node temp = first;
-            for (int i = 0; i<random; i++) {
+            for (int i = 0; i < random; i++) {
                 temp = temp.next;
             }
             return temp.item;
         }
     }                   // return (but do not remove) a random item
 
-
-
     public Iterator<Item> iterator() {
         return new QueueIterator();
     }        // return an iterator over items in order from front to end
+
+    private class Node {
+        Item item = null;
+        Node next, previous = null;
+    }
+
+    private class QueueIterator implements Iterator<Item> {
+        Node current = first;
+        Node newLast, newFirst = null;
+
+
+        public QueueIterator() {
+            int oldCount = count;
+            newFirst = new Node();
+            newLast = new Node();
+            for (int i = 0; i < oldCount; i++) {
+                Item dq = dequeue();
+                Node oldLast = newLast;
+                newLast = new Node();
+                newLast.next = null;
+                newLast.item = dq;
+                if (i == 0) {
+                    newLast.previous = null;
+                    newFirst = newLast;
+                } else {
+                    newLast.previous = oldLast;
+                    oldLast.next = newLast;
+                }
+
+            }
+            first = newFirst;
+            last = newLast;
+            count = oldCount;
+            current = first;
+            currentNode = first;
+
+        }
+
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+
+
+        }              // removeFirst and return the item from the front
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            current = current.next;
+            Item item = current.item;
+            return item;
+        }
+
+    }
 
     public static void main(String[] args) {
         RandomizedQueue<String> list = new RandomizedQueue<String>();
         list.enqueue("Dad");
         list.enqueue("Mom");
-        list.enqueue("Alyssa");
+        /*list.enqueue("Alyssa");
         list.enqueue("Jesse");
         list.enqueue("A");
         list.enqueue("B");
         list.enqueue("C");
         list.enqueue("D");
         list.enqueue("E");
-        list.enqueue("F");
+        list.enqueue("F");*/
         RandomizedQueue<String> test = list;
         Iterator<String> i = list.iterator();
 
@@ -211,11 +189,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         System.out.println("-----");
         test.dequeue();
         test.dequeue();
-        test.dequeue();
-        test.dequeue();
-        Iterator<String> j = test.iterator();
-        while (j.hasNext()) {
-            String s = j.next();
+        System.out.println("test message");
+        for (int j = 0; j< test.size(); j++){
+            System.out.println(test.currentNode.item);
+            test.currentNode = test.currentNode.next;
+        }
+        while (i.hasNext()) {
+            String s = i.next();
             System.out.println(s);
         }
         System.out.println(test.size());
